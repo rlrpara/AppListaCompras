91062bd3130a3e51f3023a04c8c1586b8dcad529
Rodrigo L Ribeiro <rlr.para@gmail.com>
2024-11-06 11:16:03 -0300

    Add project files.

---
+++ src/AppListaCompras.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppListaCompras.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/AppListaCompras.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppListaCompras.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
