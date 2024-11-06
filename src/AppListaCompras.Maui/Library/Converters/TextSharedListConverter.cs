3f90f3480dca85c4fb444cdb96e3ad5b5a09bfd6
Rodrigo L Ribeiro <rlr.para@gmail.com>
2024-11-06 17:48:22 -0300

    ajustes

---
+++ src/AppListaCompras.Maui/Library/Converters/TextSharedListConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace AppListaCompras.Maui.Library.Converters;
+
+public class TextSharedListConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        int? quantidadeUsuarios = (int?)value;
+
+        if (!quantidadeUsuarios.HasValue)
+            return null;
+
+        return quantidadeUsuarios > 1 ? "(lista compartilhada)" : string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
